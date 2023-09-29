0c45c2fa7dddeee74f289dac5cd8fac2585d9754
kentzurich <kentzurichanthony@gmail.com>
2023-09-29 09:06:35 +0800

    Register - Auth Repository

---
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Repository/AuthRepository.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using MagicVilla_CouponAPI.Data;
+using MagicVilla_CouponAPI.Models;
+using MagicVilla_CouponAPI.Models.DTO;
+using MagicVilla_CouponAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_CouponAPI.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _db;
+        private readonly IMapper _mapper;
+
+        public AuthRepository(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> IsUniqueUser(string username)
+        {
+            var user = await _db.LocalUsers.FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user == null)
+                return true;
+
+            return false;
+        }
+
+        public Task<LoginResponseDto> Authenticate(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            LocalUser user = new()
+            {
+                UserName = registrationRequestDto.UserName,
+                Password = registrationRequestDto.Password,
+                Name = registrationRequestDto.Name,
+                Role = "admin"
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+
+            user.Password = string.Empty;
+
+            return _mapper.Map<UserDto>(user);
+        }
+    }
+}
